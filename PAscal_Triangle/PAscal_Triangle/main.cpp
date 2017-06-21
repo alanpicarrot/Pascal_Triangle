59c7498e76607568554cf77a516a10646b73f2c7
Chang Yu-hsiang <yu-hsiangchang@Yu-hsiangdeMacBook-Pro.local>
2017-06-21 10:48:54 +0800

    v 0.0 github push test

---
+++ PAscal_Triangle/PAscal_Triangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAscal_Triangle
+//
+//  Created by Yu-hsiang Chang on 2017/6/21.
+//  Copyright © 2017年 Yu-hsiang Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
